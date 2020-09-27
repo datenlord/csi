a5da65d396a663f58503f54a65259fc9d537c89e
qi.tian <qit@isrc.iscas.ac.cn>
2020-09-27 00:20:49 -0700

    remove unused codes

--- src/controller.rs
+++ src/controller.rs
@@ -35,7 +35,6 @@ pub struct ControllerImpl {
 
 impl ControllerImpl {
     /// Create `ControllerImpl`
-    #[allow(dead_code)]
     pub fn new(meta_data: Arc<MetaData>) -> Self {
         let cap_vec = if meta_data.is_ephemeral() {
             Vec::new()
